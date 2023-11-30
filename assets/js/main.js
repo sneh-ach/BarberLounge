1cc089acb1a737f78c5db35dc4e2511a8256b447
Sneh Acharya <20356sneh@gmail.com>
2023-11-30 15:50:45 -0600

    Added Picture

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@
 /* 1. Proloder */
     $(window).on('load', function () {
       $('#preloader-active').delay(450).fadeOut('slow');
-      $('body').delay(450).css({
+      $('body').delay(250).css({
         'overflow': 'visible'
       });
     });
